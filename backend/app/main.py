abe404ee5ebe88fb132342d2fc9d5f10c9f0dd9e
Thai <thaiinn3@gmail.com>
2025-11-17 20:02:56 -0700

    Add simple register endpoint and user model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
+class RegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Fitness Tracker API!"}
+    return {
+        "message": "Welcome to the Fitness Tracker API!"
+    }
+
+fake_users =[]
+
+@app.post("/register")
+def register(user: RegisterRequest):
+    fake_users.append(user.email)
+    return {
+        "message": "User received!",
+        "user_email": user.email,
+        "user_password": user.password
+    }
