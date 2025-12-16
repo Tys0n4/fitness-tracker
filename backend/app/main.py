1fe286cd12be0fc54b66fdc3afbf0dce3e2c4297
Thai <thaiinn3@gmail.com>
2025-12-15 22:48:46 -0700

    Implemented JWT authentication with protected routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel, EmailStr
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+SECRET_KEY = "super-secret-key-change-later" #this goes into env later
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+security = HTTPBearer()
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security)
+    ):
+        token = credentials.credentials
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            email: str = payload.get("sub")
+
+            if email is None:
+                raise HTTPException(status_code=401, detail="Invalid token")
+            return email
+        except JWTError:
+            raise HTTPException(status_code=401, detail="Invalid token")
 
 app = FastAPI()
 
@@ -25,8 +63,7 @@ class RegisterRequest(BaseModel):
     password: str
 
 @app.get("/users")
-def get_users(email: str):
-    require_login(email)
+def get_users(current_user: str = Depends(get_current_user)):
     return users_db
 
 @app.post("/register")
@@ -48,12 +85,24 @@ def login(user: RegisterRequest):
         if stored_user["email"] == user.email:
             # 2. Check if the password matches
             if verify_password(user.password, stored_user["password"]):
-                logged_in_users.add(user.email)
-                return {"message": "Login successful!"}
-            
+                
+                access_token_expires = timedelta(
+                    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+                )
+
+                access_token = create_access_token(
+                    data={"sub": user.email},
+                    expires_delta=access_token_expires
+                )
+
+                return {
+                    "message": "login successful",
+                    "access_token": access_token,
+                    "token_type": "bearer"
+                }
+
             # Email found but password incorrect
             return {"message": "Invalid password."}
     
     # No user with that email exists
     return {"message": "User not found."}
-
